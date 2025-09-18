fe51ca4e1aff565fdec0891190ac17fd6c7a412c
I-Am-Your-GOD <166348686+I-Am-Your-GOD@users.noreply.github.com>
2025-09-18 12:29:51 -0500

    added margin

--- segmentedButtons/app/index.tsx
+++ segmentedButtons/app/index.tsx
@@ -1,41 +1,52 @@
 import React, { useState } from 'react';
+import { StyleSheet, View } from "react-native";
 import { SegmentedButtons } from 'react-native-paper';
 
 export default function App() {
   const [value, setValue] = useState('walk'); // Define state for value
 
   return (
-    <SegmentedButtons
-      value={value}
-      onValueChange={setValue}
-      density="medium"
-      theme={{ roundness: 0 }}
-      buttons={[
-        {
-          value: "walk",
-          label: "Walking",
-          icon: 'walk',
-          style: {
-            borderRadius: 0,
+    <View style={styles.container}>
+      <SegmentedButtons
+        value={value}
+        onValueChange={setValue}
+        density="medium"
+        theme={{ roundness: 0 }}
+        buttons={[
+          {
+            value: 'walk',
+            label: 'Walking',
+            icon: 'walk',
+            style: {
+              borderRadius: 0,
+            },
           },
-        },
-        {
-          value: "train",
-          label: "Transit",
-          icon: 'train',
-          style: {
-            borderRadius: 0,
+          {
+            value: 'train',
+            label: 'Transit',
+            icon: 'train',
+            style: {
+              borderRadius: 0,
+            },
           },
-        },
-        {
-          value: "drive",
-          label: "Driving",
-          icon: 'car',
-          style: {
-            borderRadius: 0,
+          {
+            value: 'drive',
+            label: 'Driving',
+            icon: 'car',
+            style: {
+              borderRadius: 0,
+            },
           },
-        },
-      ]}
-    />
+        ]}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center', 
+    margin: 20, 
+  },
+});
